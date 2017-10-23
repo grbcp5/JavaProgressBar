f403b965a1be51b369e7da3d369fed20663fffae
Grant Broadwater <grant.broadwater@garmin.com>
2017-10-23 15:47:17 -0500

    Progress bar functionality

--- src/edu/mst/grbcp5/Main.java
+++ src/edu/mst/grbcp5/Main.java
@@ -2,8 +2,57 @@ package edu.mst.grbcp5;
 
 public class Main {
 
+  public static final int PROGRESS_LEN = 20;
+
+  public static void printProgressBar( double pctComplete ) {
+
+    long complete = Math.round( PROGRESS_LEN * pctComplete );
+
+    System.out.print( "\r[" );
+
+    for( int i = 0; i < PROGRESS_LEN; i++ ) {
+
+      if( i < complete ) {
+        System.out.print( '#' );
+      } else {
+        System.out.print( ' ' );
+      }
+
+    }
+
+    System.out.print( "]" );
+
+
+    if( complete >= PROGRESS_LEN ) {
+      System.out.print( " Done" );
+    } else {
+      System.out.print( " " + ( ( int ) ( pctComplete * 100 ) ) + "%" );
+    }
+
+  }
+
   public static void main( String[] args ) {
-    System.out.println( "Hello, world!" );
+
+    double progress1;
+
+    progress1 = 0.0;
+
+    /* Iterations */
+    for ( int i = 0; i <= PROGRESS_LEN; i++ ) {
+
+      printProgressBar( progress1 );
+
+      progress1 += 1.0 / PROGRESS_LEN;
+
+      try {
+        Thread.sleep( 200 );
+      } catch ( InterruptedException e ) {
+        e.printStackTrace();
+      }
+
+    }
+
+
   }
 
 }
